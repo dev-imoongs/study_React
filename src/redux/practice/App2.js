13259607738f5105c080cffc5d07b10b3a16b272
dev-imoongs <140988974+dev-imoongs@users.noreply.github.com>
2023-09-21 13:55:32 +0900

    Delete src directory

--- src/redux/practice/App2.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import FontContainer from './FontContainer';
-
-const App2 = () => {
-  return <FontContainer />
-};
-
-export default App2;
--- src/redux/task/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import FontSizeContainer from "./task/containers/FontSizeContainer";
-
-const App = () => {
-  return <FontSizeContainer/ >
-};
-
-export default App;
